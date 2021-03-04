46614ae2cf45b40cc252d6438aeef1c2576eb0bb
MrClappy <ryan.macneille@gmail.com>
2021-03-03 21:44:22 -0800

    Create JID.sh

---
+++ Metric Scrapers/JID.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+json=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer token" http://192.168.1.6:9998/1/summary)
+
+hashrate=$(echo $json | jq -r '.hashrate.total[0]')
+workerid=$(echo $json | jq -r '.worker_id')
+algo=$(echo $json | jq -r '.algo')
+uptime=$(echo $json | jq -r '.uptime')
+
+curl -i -XPOST 'http://localhost:8086/write?db=rigs' --data-binary "xmrigs,rig=$workerid,algo=$algo uptime=$uptime,hashrate=$hashrate"

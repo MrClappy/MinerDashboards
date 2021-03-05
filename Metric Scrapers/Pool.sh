ee8645e04c99cb0cb547bbfa1e692f479ceb2607
MrClappy <ryan.macneille@gmail.com>
2021-03-04 20:29:19 -0800

    Update Pool.sh

--- Metric Scrapers/Pool.sh
+++ Metric Scrapers/Pool.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-json=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/pool/stats)
-
-getpayments=$(curl https://api.moneroocean.stream/miner/<WALLET_ID>/payments)
+getstats=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/pool/stats)
+getpayments=$(curl https://api.moneroocean.stream/miner/<ADDRESS>/payments)
+getdue=$(curl https://api.moneroocean.stream/miner/<ADDRESS>/stats)
 
 lastpayment=$(echo $getpayments | jq '.[0].ts')
 now=$(date +%s)
 timediff=(`expr $now - $lastpayment`)
+gotdue=$(echo $getdue | jq '.amtDue')
+adj=0.0
+amtdue=$(echo $adj$gotdue)
+hashRate=$(echo $getstats | jq '.pool_statistics.hashRate')
 
-hashRate=$(echo $json | jq '.pool_statistics.hashRate')
-
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=mo' --data-binary "statistics,rig=MO hashrate=$hashRate,payment=$timediff"
+curl -i -XPOST 'http://<IP>:<PORT>/write?db=mo' --data-binary "statistics,rig=MO hashrate=$hashRate,payment=$timediff,due=$amtdue"

6531c9fb24c17df313f4c333e9d8dafb90a9342e
MrClappy <ryan.macneille@gmail.com>
2021-03-14 13:42:15 -0700

    Update and rename Wemo.sh to PowerMetrics.sh

--- Metric Scrapers/Wemo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-json=$(curl -H 'Content-type:text/xml;  charset=utf-8' -H 'SOAPACTION:"urn:Belkin:service:insight:1#GetInsightParams"' -d '<?xml version="1.0" encoding="utf-8"?><s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:GetInsightParams xmlns:u="urn:Belkin:service:insight:1"></u:GetInsightParams></s:Body></s:Envelope>' 'http://<IP>:49153/upnp/control/insight1')
-
-power=$(echo $json | cut -d'|' -f8)
-
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=power' --data-binary "power value=$power"

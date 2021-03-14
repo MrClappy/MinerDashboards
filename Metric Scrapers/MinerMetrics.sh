bb28d6fe4594e3492547b94b13421b1f9d163b7b
MrClappy <ryan.macneille@gmail.com>
2021-03-14 13:40:52 -0700

    Update and rename Miner.sh to MinerMetrics.sh

---
+++ Metric Scrapers/MinerMetrics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Miner1Metrics.sh
+
+MinerStats=$(curl -X GET -H "Content-Type: application/json" http://<IP>:<PORT>/1/summary)
+
+HashRate=$(echo $MinerStats | jq -r '.hashrate.total[0]')
+Miner=$(echo $MinerStats | jq -r '.worker_id')
+Algo=$(echo $MinerStats | jq -r '.algo')
+UpTime=$(echo $MinerStats | jq -r '.uptime')
+
+curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "MinerMetrics,Miner=$Miner,Algo=$Algo UpTime=$UpTime,HashRate=$HashRate"

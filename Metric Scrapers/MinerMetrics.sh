72e6ca408085cddc47b2eae69c93249a0f9789ce
MrClappy <ryan.macneille@gmail.com>
2021-08-28 17:58:04 -0700

    Added optional GMiner support for GMiner.py

--- Metric Scrapers/MinerMetrics.sh
+++ Metric Scrapers/MinerMetrics.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 #Miner1Metrics.sh
 
+# Optional GMiner Support
+# GMiner=`python3 <PATH>/GMiner.py`
+# c=0
+# for metric in ${GMiner}; do
+#     eval "var$c=$metric";
+#    c=$((c+1));
+# done
+
+# Algo=$(echo $var0)
+# HashRate=$(echo $var1)
+# UpTime=$(echo $var3)
+
 MinerStats=$(curl -X GET -H "Content-Type: application/json" http://<IP>:<PORT>/1/summary)
 
 HashRate=$(echo $MinerStats | jq -r '.hashrate.total[0]')
